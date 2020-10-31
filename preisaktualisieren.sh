8f1b2c92f19957e2bd8c1a79f0707d44f1f3b2af
lrnzo <lrnzo@posteo.de>
2020-10-31 12:12:37 +0100

    geändert:       preisaktualisieren.sh

--- preisaktualisieren.sh
+++ preisaktualisieren.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+rm tmp.csv
 while read LINE
-	do echo "$LINE" | cut -f1
+	do myvar=$(echo "$LINE" | cut -f1)
+	if [ "$myvar" = "Artnr" ]; then
+		echo "$LINE $(date +%Y%m%d)" >> tmp.csv
+	else 
+		echo "$myvar" >> tmp.csv
+	fi
 done < Artikelnummern-bode.csv
