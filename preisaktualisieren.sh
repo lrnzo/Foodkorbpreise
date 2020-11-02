5f211ae42297a801f53fcdfa975c8df46496886f
lrnzo <lrnzo@posteo.de>
2020-11-02 07:34:56 +0100

    geändert:       Artikelnummern-bode.csv 	geändert:       preisaktualisieren.sh

--- preisaktualisieren.sh
+++ preisaktualisieren.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-rm tmp.csv
 while read LINE
-	do myvar=$(echo "$LINE" | cut -f1)
+	do myvar=$(echo "$LINE" | cut -f1 -d' ')
 	if [ "$myvar" = "Artnr" ]; then
 		echo "$LINE $(date +%Y%m%d)" >> tmp.csv
-	else 
-		echo "$myvar" >> tmp.csv
+	else
+		wget -qO "$myvar" --load-cookies=cookies.txt https://www.bodenaturkost.de/Produkt/4/"$myvar"
+		grundpreis=$(grep -A10 "id=\"tab-$myvar" "$myvar" | grep -oE 'Grundpreis: [0-9,]* ' | grep -oE '[0-9,]*')
+		echo "$LINE $grundpreis" >> tmp.csv
 	fi
 done < Artikelnummern-bode.csv
+mv tmp.csv Artikelnummern-bode.csv
